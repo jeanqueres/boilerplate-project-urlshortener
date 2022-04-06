72cc1ccb71ca1cb9085af894b8595e623fee8285
jeanqueres <jean.souzaqueres@gmail.com>
2022-04-05 22:33:55 -0300

    first commit

--- server.js
+++ server.js
@@ -10,15 +10,33 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+// URL Shortener Microservice POST
+app.post('/api/shorturl', (req, res, next) => {
+  res.json({ body: req.body });
+  //   original_url: req.body,
+  //   short_url: 
+  // });
+});
+
+// URL Shortener Microservice GET
+app.get('/api/shorturl/short_url?', function (req, res) {
+  let url = req.params['short_url'];
+
+  if (err)
+
+    res.redirect(url);
+
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
